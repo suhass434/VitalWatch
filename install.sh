922f020d0715f5da5f9d90bd712f95e4cef56692
suhass434 <1ms22is139@msrit.edu>
2024-10-30 17:49:19 +0530

    updated installer to work in fish

--- install.sh
+++ install.sh
@@ -1,3 +1,33 @@
+#!/bin/bash
+
+# mkdir -p ~/.local/share/AutoGuard
+
+# python -m venv ~/.local/share/AutoGuard/venv
+
+# source ~/.local/share/AutoGuard/venv/bin/activate.fish
+
+# pip install -r requirements.txt
+
+# mkdir -p ~/.local/share/system-monitor/app
+# cp -r app/* ~/.local/share/system-monitor/app/
+# cp run.py ~/.local/share/system-monitor/
+
+# cp AutoGuard.desktop ~/.local/share/applications/
+
+# chmod +x ~/.local/share/AutoGuard.desktop/run.py
+
+# echo "Installation completed successfully."
+
+# python run.py
+
+
+
+# Check if the current shell is fish
+if [ "$SHELL" != "/usr/bin/bash" ] || [ "$SHELL" != "/bin/bash" ]; then
+    echo "Switching to bash to avoid fish shell issues..."
+    exec bash "$0"  # Restart this script in bash
+fi
+
 python -m venv env
 source env/bin/activate
 
